175dc805f1fc17f8a4364142725deb10de9bc704
Dale Short <short.dale@gmail.com>
2022-12-01 22:03:19 -0500

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,20 @@
+import heapq
+
+file_path = "foodlist.txt"
+f = open(file_path, "r")
+calSum = 0
+maxCalList = []
+
+for line in f:
+    if(line == '\n'):
+        if(len(maxCalList) <3):  
+            heapq.heappush(maxCalList,calSum)
+        elif(calSum > maxCalList[0]):
+            heapq.heappop(maxCalList)
+            heapq.heappush(maxCalList,calSum)
+        calSum = 0
+
+    else:
+        calSum += int(line)
+
+print(sum(maxCalList))
